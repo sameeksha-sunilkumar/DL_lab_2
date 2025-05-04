647a5aa50786774aa1f8fa5a932fb5d41a9153b9
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-05-04 21:20:04 +0530

    program 4

---
+++ autoencoder.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+from tensorflow.keras.datasets import mnist
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+class SimpleAutoencoder(Model):
+    def __init__(self, latent_dimensions):
+        super(SimpleAutoencoder, self).__init__()
+        self.encoder = tf.keras.Sequential([
+            layers.Input(shape=(28, 28, 1)),
+            layers.Flatten(),
+            layers.Dense(latent_dimensions, activation='relu'),
+        ])
+        self.decoder = tf.keras.Sequential([
+            layers.Dense(28 * 28, activation='sigmoid'),
+            layers.Reshape((28, 28, 1))
+        ])
+
+    def call(self, input_data):
+        encoded = self.encoder(input_data)
+        decoded = self.decoder(encoded)
+        return decoded
+latent_dimensions = 64
+autoencoder = SimpleAutoencoder(latent_dimensions)
+autoencoder.compile(optimizer='adam', loss='mean_squared_error')
+autoencoder.fit(x_train, x_train,
+                epochs=10,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+encoded_imgs = autoencoder.encoder(x_test).numpy()
+decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+n = 6
+plt.figure(figsize=(12, 6))
+for i in range(n):
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28), cmap='gray')
+    plt.title("og")
+    plt.axis('off')
+
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28), cmap='gray')
+    plt.title("Reconstructed")
+    plt.axis('off')
+    plt.show()
