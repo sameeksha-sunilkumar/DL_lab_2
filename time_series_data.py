eac71260c6c4730a67961b98458bac9feca4b563
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-05-05 20:24:54 +0530

    program 2

--- time_series_data.py
+++ time_series_data.py
@@ -1,39 +1,41 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dropout, Dense
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
 data = np.sin(np.arange(0, 100, 0.1)).reshape(-1, 1)
 
-sc = MinMaxScaler(feature_range=(0, 1))
-scaled_data = sc.fit_transform(data)
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data)
 
-x_train = []
-y_train = []
 time_step = 10
+x, y = [], []
 for i in range(time_step, len(scaled_data)):
-    x_train.append(scaled_data[i-time_step:i, 0])
-    y_train.append(scaled_data[i, 0])
+    x.append(scaled_data[i - time_step:i, 0])
+    y.append(scaled_data[i, 0])
 
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+x, y = np.array(x), np.array(y)
+x = x.reshape(x.shape[0], x.shape[1], 1)
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(tf.keras.layers.Dropout(0.2))
-model.add(tf.keras.layers.LSTM(units=50))
-model.add(tf.keras.layers.Dropout(0.2))
-model.add(tf.keras.layers.Dense(units=1))
+train_size = int(len(x) * 0.8)
+x_train, x_test = x[:train_size], x[train_size:]
+y_train, y_test = y[:train_size], y[train_size:]
 
+model = Sequential()
+model.add(LSTM(50,return_sequences=True,input_shape=(time_step,1)))
+model.add(Dropout(0.2))
+model.add(LSTM(50,return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=50, batch_size=32)
+model.fit(x_train, y_train, epochs=100, batch_size=32)
 
-predicted = model.predict(x_train)
-predicted = sc.inverse_transform(predicted)
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
 
-plt.plot(data, label='Original')
-plt.plot(predicted, label='Predicted', color='red')
+plt.plot(data, label='Original Data', color='blue')
+plt.plot(np.arange(train_size + time_step, len(data)), predictions, label='Predicted Data', color='red')
 plt.legend()
-plt.title('Sine Wave Prediction')
 plt.show()
