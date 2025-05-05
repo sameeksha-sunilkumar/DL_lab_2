f9f611a174ab80b96d4baefd9820a0eb34d9a708
Sameeksha <160415587+sameeksha-sunilkumar@users.noreply.github.com>
2025-05-05 18:51:05 +0530

    program 4

--- google_stock.py
+++ google_stock.py
@@ -3,22 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
-
 data = pd.read_csv('GOOG.csv')
 dataset = data[['open']].values  
-
 sc = MinMaxScaler(feature_range=(0, 1))
 scaled_data = sc.fit_transform(dataset)
-
 x_train = []
 y_train = []
 for i in range(60, len(scaled_data)):
     x_train.append(scaled_data[i-60:i, 0])
     y_train.append(scaled_data[i, 0])
-
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
-
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(tf.keras.layers.Dropout(0.2))
@@ -29,6 +24,5 @@ model.add(tf.keras.layers.Dropout(0.2))
 model.add(tf.keras.layers.LSTM(units=50))
 model.add(tf.keras.layers.Dropout(0.2))
 model.add(tf.keras.layers.Dense(units=1))
-
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=50, batch_size=32)
